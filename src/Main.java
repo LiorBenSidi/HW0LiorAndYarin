64aa0784cbad68fe659ed36bb29aed7def779e4a
Lior <128314109+LiorTechnion@users.noreply.github.com>
2023-04-21 21:59:25 +0300

    הוספתי תיעוד לקוד, שורת ועמודת מספרים בכל לוח(שורה ועמודה ראשונה), אולם, נשאר לשים "רווח" במיקום 0,0 בכל לוח. בנוסף, שיניתי את הלולאה שבשורה 66(כרגע) ללואת  "for each"  שעוברת על כל איבר שבתת-מערך(שורה) במערך "arr".  כמו כן, הוספתי שתי לולאות כדי למקם את הספינות בכל לוח,  אולם, נשאר לוודא את ההיתכנות של מיקום הספינות באמצעות תנאים שנוסיף.

--- src/Main.java
+++ src/Main.java
@@ -9,46 +9,72 @@ public class Main {
 
     public static void battleshipGame() {
         // TODO: Add your code here (and add more methods).
-        Scanner scanner1 = new Scanner(System.in);
+        /** creating the boards.
+         * We get the board size from the user,
+         * Create 4 boards (2 for the player and 2 for the computer, when there are 2 board types: game and guessing),
+         * and initialize the boards according to the requirements. */
+        /**<p>*/Scanner scanner1 = new Scanner(System.in);
         System.out.println("Enter the board size");
         String boardSize = scanner1.nextLine();
         final int RAW = Integer.parseInt(String.valueOf(boardSize.charAt(0)));
-        final int COLUME = Integer.parseInt(String.valueOf(boardSize.charAt(2)));
-        int[][] playersGameBoard = new int[RAW+1][COLUME+1];
-        int[][] computersGameBorad = new int[RAW+1][COLUME+1];
-        int[][] playersGuessBorad = new int[RAW+1][COLUME+1];
-        int[][] computersGuessBorad = new int[RAW+1][COLUME+1];
-        for (int i = 0; i< RAW; i++){
-            for (int j = 0; j < COLUME; j++){
+        final int COLUMN = Integer.parseInt(String.valueOf(boardSize.charAt(2)));
+        int[][] playersGameBoard = new int[RAW+1][COLUMN+1];
+        int[][] playersGuessBoard = new int[RAW+1][COLUMN+1];
+        int[][] computersGameBoard = new int[RAW+1][COLUMN+1];
+        int[][] computersGuessBoard = new int[RAW+1][COLUMN+1];
+        for (int i = 1; i < RAW + 1; i++){
+            for (int j = 1; j < COLUMN + 1; j++){
                 playersGameBoard[i][j] = '–';
-                playersGuessBorad[i][j] = '–';
-                computersGameBorad[i][j] = '–';
-                computersGuessBorad[i][j] = '–';
+                playersGuessBoard[i][j] = '–';
+                computersGameBoard[i][j] = '–';
+                computersGuessBoard[i][j] = '–';
             }
         }
+          // TODO: Inserting an empty space at position 0,0 of each board.
+//        String emptySpace = " ";
+//        playersGameBoard[0][0] = Integer.parseInt(String.valueOf(emptySpace.charAt(0)));
+//        playersGuessBoard[0][0] = Integer.parseInt(String.valueOf(emptySpace.charAt(0)));
+//        computersGameBoard[0][0] = Integer.parseInt(String.valueOf(emptySpace.charAt(0)));
+//        computersGuessBoard[0][0] = Integer.parseInt(String.valueOf(emptySpace.charAt(0)));
+        for (int i = 1; i < COLUMN + 1; i++){
+            playersGameBoard[0][i] = i - 1;
+            playersGuessBoard[0][i] = i - 1;
+            computersGameBoard[0][i] = i - 1;
+            computersGuessBoard[0][i] = i - 1;
+        }
+        for (int i = 1; i < RAW + 1; i++){
+            playersGameBoard[i][0] = i - 1;
+            playersGuessBoard[i][0] = i - 1;
+            computersGameBoard[i][0] = i - 1;
+            computersGuessBoard[i][0] = i - 1;
+        }
 
-        System.out.println("Enter the battleships size");
+        /** The number of battleships and their size.*/
+        /**<p>*/System.out.println("Enter the battleships size");
         String battleshipSizes = scanner1.nextLine();
-        int numberOfBattelships = ((battleshipSizes.length()) / 4) + 1;
-        int[][] arr = new int[numberOfBattelships][2];
+        int numberOfBattleships = ((battleshipSizes.length()) / 4) + 1;
+        int[][] arr = new int[numberOfBattleships][2];
         int j = 0;
         for (int i = 0; i <= (battleshipSizes.length() - 2); i += 4) {
             arr[j][0] = Integer.parseInt(String.valueOf(battleshipSizes.charAt(i)));
             arr[j][1] = Integer.parseInt(String.valueOf(battleshipSizes.charAt(i + 2)));
             j++;
         }
-        boolean massageFlag = true;
-        for (int i = 0; i < arr.length; i++) {
-            int k = arr[i][0];
-            do{
-                if(massageFlag) {
-                    System.out.println("Enter location and orientation for battleship of size " + arr[i][1]);
+
+        /**Location of user submarines.*/
+        /**<p>*/boolean massageFlag = true;
+        for (int[] ints : arr) {
+            int numOfShipsOfTheSameSize = ints[0];
+            int SizeOfShips = ints[1];
+            do {
+                if (massageFlag) {
+                    System.out.println("Enter location and orientation for battleship of size " + SizeOfShips);
                 }
                 String xyo = scanner1.nextLine();
                 int x = Integer.parseInt(String.valueOf(xyo.charAt(0)));
                 int y = Integer.parseInt(String.valueOf(xyo.charAt(3)));
                 int o = Integer.parseInt(String.valueOf(xyo.charAt(6)));
-                if (x > COLUME || y > RAW){
+                if (x > COLUMN || y > RAW) {
                     System.err.println("Illegal tile, try again!");
                     massageFlag = false;
                 } else if (o != 0 && o != 1) {
@@ -56,10 +82,21 @@ public class Main {
                     massageFlag = false;
                 } else {
                     massageFlag = true;
-                    k--;
+                    if (o == 0){
+                        for (int i = y + 1; i < (y + SizeOfShips + 1); i++){
+                            playersGameBoard[x + 1][i] = '#';
+                        }
+                    }
+                    if (o == 1){
+                        for (int i = x + 1; i < (x + SizeOfShips + 1); i++){
+                            playersGameBoard[i][y] = '#';
+                        }
+                    }
+                    numOfShipsOfTheSameSize--;
+                    // TODO: Enforce the integrity of battleship's location through conditions.
                 }
+            } while (numOfShipsOfTheSameSize > 0);
 
-            }while(k>0);
         }
     }
 
