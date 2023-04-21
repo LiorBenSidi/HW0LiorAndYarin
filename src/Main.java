bb04b6cece007352d141134ad6d2fd47881ab609
Yarin Katan <yarin.katan@campus.technion.ac.il>
2023-04-21 18:34:21 +0300

    נשאר לנו למקמם את הצוללות תוך התחשבות בתנאים של המשחק

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,60 @@ public class Main {
 
     public static void battleshipGame() {
         // TODO: Add your code here (and add more methods).
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Enter the board size");
+        String boardSize = scanner1.nextLine();
+        final int RAW = Integer.parseInt(String.valueOf(boardSize.charAt(0)));
+        final int COLUME = Integer.parseInt(String.valueOf(boardSize.charAt(2)));
+        int[][] playersGameBoard = new int[RAW+1][COLUME+1];
+        int[][] computersGameBorad = new int[RAW+1][COLUME+1];
+        int[][] playersGuessBorad = new int[RAW+1][COLUME+1];
+        int[][] computersGuessBorad = new int[RAW+1][COLUME+1];
+        for (int i = 0; i< RAW; i++){
+            for (int j = 0; j < COLUME; j++){
+                playersGameBoard[i][j] = '–';
+                playersGuessBorad[i][j] = '–';
+                computersGameBorad[i][j] = '–';
+                computersGuessBorad[i][j] = '–';
+            }
+        }
+
+        System.out.println("Enter the battleships size");
+        String battleshipSizes = scanner1.nextLine();
+        int numberOfBattelships = ((battleshipSizes.length()) / 4) + 1;
+        int[][] arr = new int[numberOfBattelships][2];
+        int j = 0;
+        for (int i = 0; i <= (battleshipSizes.length() - 2); i += 4) {
+            arr[j][0] = Integer.parseInt(String.valueOf(battleshipSizes.charAt(i)));
+            arr[j][1] = Integer.parseInt(String.valueOf(battleshipSizes.charAt(i + 2)));
+            j++;
+        }
+        boolean massageFlag = true;
+        for (int i = 0; i < arr.length; i++) {
+            int k = arr[i][0];
+            do{
+                if(massageFlag) {
+                    System.out.println("Enter location and orientation for battleship of size " + arr[i][1]);
+                }
+                String xyo = scanner1.nextLine();
+                int x = Integer.parseInt(String.valueOf(xyo.charAt(0)));
+                int y = Integer.parseInt(String.valueOf(xyo.charAt(3)));
+                int o = Integer.parseInt(String.valueOf(xyo.charAt(6)));
+                if (x > COLUME || y > RAW){
+                    System.err.println("Illegal tile, try again!");
+                    massageFlag = false;
+                } else if (o != 0 && o != 1) {
+                    System.err.println("Illegal orientation, try again!");
+                    massageFlag = false;
+                } else {
+                    massageFlag = true;
+                    k--;
+                }
+
+            }while(k>0);
+        }
     }
+
 
 
     public static void main(String[] args) throws IOException {
